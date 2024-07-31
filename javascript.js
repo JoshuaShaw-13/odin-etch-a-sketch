3d8de22dab89bcc30e602284b29e86767c2f328b
Joshua-Shaw <141381258+JoshuaShaw-13@users.noreply.github.com>
2024-07-31 13:29:57 -0600

    edit how creating new grid works
    before the user would enter both width and height
    resulting in some grids not being squares, now
    users enter only one number which is used to
    create squares only

--- javascript.js
+++ javascript.js
@@ -1,36 +1,34 @@
-const GRID_WIDTH = 640;
-const GRID_HEIGHT = 640;
+const GRID_SIZE = 640;
 
 const grid = document.querySelector("#grid");
 const new_grid_btn = document.querySelector("#new_grid");
 
 new_grid_btn.addEventListener("click", () => {
-    let width = prompt("Enter new width: ");
-    let height = prompt("Enter new height: ");
+    let size = prompt("Enter new square size (1-100): ");
+    while (size < 1 || size > 100) {
+        size = prompt("Invalid Entry! \n Enter new square size (1-100): ")
+    };
     
     while (grid.firstChild) {
         grid.removeChild(grid.firstChild);
     };
-    createNewGrid(width, height);
+    createNewGrid(size);
 });
 
-function createNewGrid(width = 16, height = 16) {
-    let square_width = GRID_WIDTH/width;
-    let square_height = GRID_HEIGHT/height;
-    console.log(square_height);
-    console.log(square_width);
-    
+function createNewGrid(size = 16) {
+    let square_size = GRID_SIZE/size;
+
     i = 0;
-    while (i < height) {
+    while (i < size) {
         const new_row = document.createElement("div");
         new_row.className = "row";
         j = 0;
-        while (j < width) {
+        while (j < size) {
             const new_div = document.createElement("div");
             new_div.className = "square";
 
-            new_div.style.width = square_width + "px";
-            new_div.style.height = square_height + "px";
+            new_div.style.width = square_size + "px";
+            new_div.style.height = square_size + "px";
 
             if ((i+j)%2 == 0) {
                 new_div.style.backgroundColor = 'gray';
